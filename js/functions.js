d4d2fb35912415c410b75afa3911b809c5381cbd
Grafikart <contact@grafikart.fr>
2014-10-17 00:24:45 +0200

    First commit for page

--- js/functions.js
+++
@@ -1,8 +0,0 @@
-function fuzzy(value, search){
-    var regexp = '\\b(.*)';
-    for(var i in search){
-        regexp += '('+search[i]+')(.*)';
-    }
-    regexp += '\\b';
-    return value.match(new RegExp(regexp,'i'));
-}
